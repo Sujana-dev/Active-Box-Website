f6e07ccc88c9d5dd352f05c5274d638f3508248d
Sujana-dev <sujanaa482@gmail.com>
2024-01-28 19:59:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.info_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    centerMode:true,
+    centerPadding:0
+  });
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    centerMode:true,
+    centerPadding:0,
+    dots:true,
+  });
